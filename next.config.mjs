2d5a2ba75a8e96b6dce1994ea55e8ceb3a8aff85
Rohan Nimje <rohannimje53@gmail.com>
2026-07-12 23:39:02 +0530

    Update next.config.mjs for export and basePath

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,25 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   typescript: {
+//     ignoreBuildErrors: true,
+//   },
+//   images: {
+//     unoptimized: true,
+//   },
+// }
+
+// export default nextConfig
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  basePath: '/portfolio',
   typescript: {
     ignoreBuildErrors: true,
   },
   images: {
     unoptimized: true,
   },
-}
+};
 
-export default nextConfig
+export default nextConfig;
